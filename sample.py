713b1387dfe50364d3e309f86bdc636169250f1b
Vaibhavmulti <vaibhavmulti@gmail.com>
2024-07-11 13:30:35 +0530

    Sample moves for valid_moves checking

--- sample.py
+++ sample.py
@@ -10,14 +10,14 @@ from model import GPTConfig, GPT
 
 #Specify your model name here
 MODEL_NAME = "CheckersHuman.pt"
-SAMPLE_DUMP_NAME = "data/checkers_games/sample_dump/sample_dump"         #For checking the valid moves
+SAMPLE_DUMP_NAME = "data/checkers_games/sample_dump/"         #For checking the valid moves
 META_PATH = "data/checkers_games/meta.pkl"
 # -----------------------------------------------------------------------------
 init_from = 'resume' # either 'resume' (from an out_dir) or a gpt2 variant (e.g. 'gpt2-xl')
 out_dir = 'out-shakespeare-char' # ignored if init_from is not 'resume'
 start = "\n" # or "<|endoftext|>" or etc. Can also specify a file, use as: "FILE:prompt.txt"
 #start = "FILE:prompt.txt" 
-num_samples = 100 # number of samples to draw
+num_samples = 1 # number of samples to draw
 max_new_tokens = 400 #500 # number of tokens generated in each sample
 temperature = 0.8 # 1.0 = no change, < 1.0 = less random, > 1.0 = more random, in predictions
 top_k = 200 # retain only the top_k most likely tokens, clamp others to have 0 probability
@@ -100,9 +100,8 @@ with torch.no_grad():
 import pickle
 
 # Pickle the list
-with open(SAMPLE_DUMP_NAME+'.pkl', 'wb') as f:
+os.makedirs(SAMPLE_DUMP_NAME, exist_ok=True)
+with open(SAMPLE_DUMP_NAME+'sample_dump.pkl', 'wb') as f:
     pickle.dump(listofdecodes, f)
-
-
 
 
